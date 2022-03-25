ea7ae16d8867d54b3d2c45272839d3475dd3205d
favremp4 <simon.favree@gmail.com>
2022-03-25 16:00:47 +0100

    A^jout de l'article 1

--- article-1/article-1.js
+++ article-1/article-1.js
@@ -2,7 +2,28 @@ import { DragElement } from "../components/DragElement"
 
 
 import "../css/reset.css";
-import "../css/style.css";
+/*import "../css/style.css";*/
+import "../css/article.css";
 
 let img = document.querySelector("img");
 DragElement(img);
+
+const navigation = document.querySelector('.navigation');
+
+const navigationOffsetTop = navigation.offsetTop;
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY<= 800) {
+        
+        
+        navigation.style.visibility='hidden';
+       
+        
+    }
+    else{
+        navigation.style.visibility='visible';
+      
+      
+    }
+});
+
