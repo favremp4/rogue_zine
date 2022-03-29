f79a03f7a1010c14b43d3d2aaf0ce4ac6191a80c
dark-sasuke-12 <96484772+dark-sasuke-12@users.noreply.github.com>
2022-03-29 14:16:47 +0200

    cre article 4
    backup article bugs

--- article-4/article-1.js
+++ article-4/article-1.js
@@ -9,10 +9,47 @@ let img = document.querySelector(".draggable1");
 DragElement(img);
 let img2 = document.querySelector(".draggable2");
 DragElement(img2);
-let img3 = document.querySelector(".draggable3");
-DragElement(img3);
-let img4 = document.querySelector(".draggable4");
-DragElement(img4);
+
+
+
+
+
+
+const abces = document.querySelector('.abces');
+
+const abcesOffsetTop = abces.offsetTop;
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY<= 800) {
+        
+        
+        abces.style.visibility='hidden';
+       
+        
+    }
+    else{
+        abces.style.visibility='visible';
+      
+      
+    }
+});
+
+
+//----------------------------------------------------------------------
+//----------------------------------------------------------------------
+//----------------------------------------------------------------------
+
+
+
+
+// let img = document.querySelector(".draggable1");
+// DragElement(img);
+// let img2 = document.querySelector(".draggable2");
+// DragElement(img2);
+// let img3 = document.querySelector(".draggable3");
+// DragElement(img3);
+// let img4 = document.querySelector(".draggable4");
+// DragElement(img4);
 
 
 
@@ -46,21 +83,21 @@ const atroisOffsetTop = atrois.offsetTop;
 const article = document.querySelector('.article');
 const bande = document.querySelector('.bande');
 
-const abces = document.querySelector('.abces');
+// const abces = document.querySelector('.abces');
 
 
-//---------------------------------------------------
+// //---------------------------------------------------
 
-const abcesOffsetTop = abces.offsetTop;
+// const abcesOffsetTop = abces.offsetTop;
 
-window.addEventListener('scroll', () => {
-    if(window.scrollY<= 800) {                
-        abces.style.visibility='hidden'; 
-    }
-    else{
-        abces.style.visibility='visible';            
-    }
-});
+// window.addEventListener('scroll', () => {
+//     if(window.scrollY<= 800) {                
+//         abces.style.visibility='hidden'; 
+//     }
+//     else{
+//         abces.style.visibility='visible';            
+//     }
+// });
 
 //----------------------------------------------------
 
